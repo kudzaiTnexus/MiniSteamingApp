468e29b09683272ad16f89c64f8c370950438d1f
Kudzai <kudzaimhou@gmail.com>
2022-07-04 16:55:12 +0200

    Initial Commit

---
+++ MiniSteamingApp/MiniSteamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSteamingAppApp.swift
+//  MiniSteamingApp
+//
+//  Created by Kudzaishe Mhou on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MiniSteamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
